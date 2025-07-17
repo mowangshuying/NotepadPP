5140ae0fa262ea1e53109e1094d1b7e0156e13d6
mowangshuying <1911364602@qq.com>
2025-07-18 07:25:28 +0800

    add qscintilla

--- FileManager.h
+++ FileManager.h
@@ -2,6 +2,19 @@
 
 #include <QObject>
 
+
+const int MaxFileSize = 1024 * 1000;
+
+enum class ErrorType {
+    NoError,
+    OpenEmptyFile,
+};
+
+class NewFileIdMgr {
+    public:
+        int nIndex;
+};
+
 class FlieManager : public QObject 
 {
         Q_OBJECT
@@ -15,11 +28,16 @@ class FlieManager : public QObject
         {
 
         }
-        
+
     public:
         static FlieManager& getInstance()
         {
             static FlieManager instance;
             return instance;
+        }
+
+        int getNextNewFileId()
+        {
+            
         }
 };
